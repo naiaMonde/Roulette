8c01ea163a0cad237b66b0c39c42593d7246cc2b
Naia Mondeteguy <nmondeteguy@iutbayonne.univ-pau.fr>
2026-04-05 18:05:04 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# README
+
+## Informations générales
+
+La roulette est un site qui permet de choisir aléatoirement un film dans les données letterboxd d'un utilisateur
+
+## Installations requises
+
+```bash
+npm install
+```
+
+```bash
+composer install
+```
---
+++ include.php
@@ -0,0 +1,10 @@
+<?php
+
+// Ajout de twig
+require_once 'config/twig.php';
+require_once 'config/config.php';
+
+//Ajout des controlleurs
+require_once 'controllers/controller.class.php';
+require_once 'controllers/controller_utilisateur.class.php';
+require_once 'controllers/controller_factory.class.php';
